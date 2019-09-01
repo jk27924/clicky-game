d4da4be48285488b4dc09dbb86add13ce329a50e
Jihong Kim <jk279242naver.com>
2019-08-31 23:19:34 -0500

    edited some comments for new changes with better and more detailed explanation, AND currently working on writing a function that can store clicked friends ids inside of an array of clickedFriends.

--- src/App.js
+++ src/App.js
@@ -5,31 +5,38 @@ import Title from "./components/Title";
 import friends from "./friends.json";
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
+
   state = {
     friends,
+      // Setting this.state.friends to the friends json array
     clickedFriends: [],
-    // this is to hold the ids of clicked friends in an array.
+      // This is to hold the ids of clicked friends in an array.
     topScore: 0,
     currentScore: 0
-    // put these two scores in state object, because they keep changing.
+      // Put these two scores in state object, because they keep changing.
   };
 
   shuffleFriend = (friend) => {
-  // Switched (friends) to (friend), and why?
+    // Switched (friends) to (friend), because I use friend to pass component props to the function, and friends is the actual props from a component.
 
-  // Use 'for loop' to randomize the order of friend cards when showing on the DOM / math.random will help randomizing / add math.floor to round up to avoid gettin zero / i + 1 is also need to avoid getting zero.
+  // TO DO: Write a small function that checks if a friend has already been clicked, and those clicked ones' ids need to store inside of the array clickedFriends.
+    // const idClicked = this.state.clickedFriends.filter(friend => friend.id);
+    const idClicked = this.state.clickedFriends.filter(friend => friend.id);
+    console.log(idClicked);
+
+
   console.log(friend);
 
   const newFriends = this.state.friends;
   // Thought I am not supposed to use const, since the cards are changing with onClick function. BUT since it is just shifting items around the same array, Using const would not matter.
 
+    // Use 'for loop' to randomize the order of friend cards when showing on the DOM / math.random will help randomizing / add math.floor to round up to avoid gettin zero / i + 1 is also need to avoid getting zero.
     for (var i = newFriends.length - 1; i > 0; i--) {
       console.log(i);
 
       var randomOrder = Math.floor(Math.random() * (i + 1));
       var holder = newFriends[i];
-      // friends[i]: it hold the actual data that I am using, which is friends.
+      // newFriends[i]: it hold the actual data that I am using, which is newFriends.
       newFriends[i] = friends[randomOrder];
       newFriends[randomOrder] = holder;
       console.log(holder);
