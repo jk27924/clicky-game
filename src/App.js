99644607d699030fbb2b424e9186101dc67c4aac
Jihong Kim <jk279242naver.com>
2019-09-02 03:33:48 -0500

    if/else statement does what it is supposed to do with incrementing per each click. When the length of the array becomes 11, which is Max, it returns to 0. And when the id of clicked card is already in the array, it empties the array. Current Score and Top score are also working good.

--- src/App.js
+++ src/App.js
@@ -18,68 +18,50 @@ class App extends Component {
   shuffleFriend = (friend) => {
     // Switched (friends) to (friend), because I use friend to pass component props to the function, and friends is the actual props from a component.
 
-    // A Function that checks if a friend has already been clicked, and those clicked ones' ids need to store inside of the array clickedFriends.    
+    // A Function Below checks if a friend has already been clicked, and those clicked ones' ids need to store inside of the array clickedFriends.    
+
     const addClickedFriends = this.state.clickedFriends;
-    // addClickedFriends.push(friend.id);
-
-    this.setState({
-      clickedFriends: addClickedFriends
-    })
-      // This changes the state to addClickedFriends..
-
-    console.log (addClickedFriends);
+    // console.log (addClickedFriends);
       // Returns an array with clicked cards' ids stored inside.
 
-    // console.log(friend.id);
-      // This returns the id of selected card, NOT in an array.
-
-    let clickedFriends = [];
-
-    if (clickedFriends.indexOf(friend.id) === -1) {
+    if (addClickedFriends.indexOf(friend.id) === -1) {
       // If the clicked ID is NOT in the array,
 
       addClickedFriends.push(friend.id)
+      // Adding ids of clicked friend cards into an array
 
       this.setState({
       currentScore: this.state.currentScore + 1,
+      // Incrementing per each click for current score
+
       topScore: this.state.currentScore + 1 > this.state.topScore ? this.state.currentScore + 1: this.state.topScore
+      // Ternary: Using ? means IF statement before ? is TRUE, the left-hand will work. ELSE the right-hand will work.
       })
 
     } else {
       // If the clicked ID is already in the array,
 
        this.setState({ 
-        clickedFriends: [],
-        currentScore: 0,
+        clickedFriends: [], // The array becomes empty
+        currentScore: 0, // The Current Score will be back to ZERO
       })
       console.log("You Lost");
-      return;
     };
     
 
 
-
-
-
-
-
-
-
   const newFriends = this.state.friends;
-  // Thought I am not supposed to use const, since the cards are changing with onClick function. BUT since it is just shifting items around the same array, Using const would not matter.
-
-    // Use 'for loop' to randomize the order of friend cards when showing on the DOM / math.random will help randomizing / add math.floor to round up to avoid gettin zero / i + 1 is also need to avoid getting zero.
+  // Const is used, B/C it is just shifting items around the same array, and it is not changing the actual array itself.
+    // FOR LOOP is used to randomize the order of friend cards when showing on the DOM
+    // math.random randomizes / math.floor rounds up to avoid gettin zero
     for (var i = newFriends.length - 1; i > 0; i--) {
-      // console.log(i);
 
       var randomOrder = Math.floor(Math.random() * (i + 1));
       var holder = newFriends[i]; // newFriends[i]: it hold the actual data that I am using, which is newFriends.
       newFriends[i] = friends[randomOrder];
       newFriends[randomOrder] = holder;
-      // console.log(holder);
-      // This returns all object info on console.
     }
-    // return friends;
+
     this.setState({'friends': newFriends});
   };
 
@@ -96,10 +78,10 @@ class App extends Component {
         <Title>Clicky Game with Friends</Title>
         <h2>Click on an image to earn points, but don't click on any more than once.</h2>
         <br/>
-        <h3 className="scoreBoard">
+        <h2 className="scoreBoard">
           Current Score: {this.state.currentScore}  | 
           Top Score: {this.state.topScore}
-        </h3>
+        </h2>
 
         {this.state.friends.map(friend => (
           <FriendCard
